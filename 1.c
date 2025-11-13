51287a53525d2df8724309e2299a49a8c6b75232
RNaveen <rnaveenkumar1933@gmail.com>
2025-11-13 14:57:47 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a,b,sum;
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+sum=a+b;
+printf("Sum is %d\n",sum);
+return 0;
+}
+//addition of two numbers
---
+++ 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int main(){
+ int a,b,multi;
+ printf("enter numbers");
+ scanf("%d %d",&a,&b);
+ multi=a*b;
+ printf("Multiplication is %d\n",multi);
+ return 0;
+}
+//Multiplication of two numbers
